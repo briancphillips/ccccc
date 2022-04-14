646c157573d0c28ee61a5fe996246f0d8157319f
Brian Phillips <brian@briancphillips.com>
2022-04-14 15:57:39 -0500

    googlely moogily

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ class Ball {
     this.x = x;
     this.y = y;
     this.r = r;
-    this.dx = -4;
+    this.dx = 4;
     this.dy = -4;
   }
 
@@ -49,13 +49,28 @@ class Ball {
 
     //collisions with paddle
 
+    // if(this.y+this.r>ship.y && this.dy>0) {
+
+    //   if(this.x+this.r>=ship.x && this.x-this.r<=ship.x+ship.w) {
+    //     this.dy*=-1;
+    //     this.y+=this.dy;
+    //   }
+
+    // }
+
     if (
       this.x + this.r >= ship.x &&
-      this.x + this.r <= ship.x + ship.w &&
+      this.x - this.r <= ship.x + ship.w &&
       this.y + this.r >= ship.y &&
-      this.y+this.r<=ship.y+ship.h
+      this.y - this.r <= ship.y + ship.h &&
+      this.dy > 0
     ) {
+      if (this.x + this.r >= ship.x + ship.w - ship.w * 0.25 && this.dx < 0)
+        this.dx *= -1;
+      if (this.x + this.r <= ship.x + ship.w * 0.25 && this.dx > 0)
+        this.dx *= -1;
       this.dy *= -1;
+      //this.y += this.dy;
     }
   }
 
@@ -82,9 +97,23 @@ class Ship {
   }
 
   draw() {
+    let x = this.x + this.w - this.w * 0.25;
+    let x2 = this.x + this.w * 0.25;
+    let y = this.y;
+    let y2 = this.y + this.h;
+    ctx.beginPath();
     ctx.lineWidth = 2;
-    ctx.fillStyle = "rgba(255,0,0,100)";
+    ctx.fillStyle = "rgba(128,128,128,100)";
     ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.moveTo(x, y);
+    ctx.strokeStyle = "red";
+    ctx.lineTo(x, y2);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(x2, y);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
   }
   update() {
     if (this.x < 0) this.x = 0;
@@ -94,42 +123,51 @@ class Ship {
     this.draw();
   }
 }
+let fps = 0;
+let startTime = 0;
+let frame = 0;
+let ship;
+let ball;
+let bricks;
+init();
 
-let ship = new Ship(100, 100, 100, 20);
-let ball = new Ball(200, 450, 5);
-//let brick=new Brick(10,10,50,10,'rgba(0,0,255,1)');
+function init() {
+  ship = new Ship(50, 100, 100, 20);
+  ball = new Ball(200, 450, 5);
+  let colors = ["red", "orange", "yellow", "green"];
+  let color = colors[0],
+    colorIndex = 0;
+  bricks = [];
+  let brickW = 75;
+  let brickH = 20;
 
-let colors = ["red", "purple", "orange", "green"];
-let color = colors[0],
-  colorIndex = 0;
-let bricks = [];
-let brickW = 75;
-let brickH = 20;
+  let cols = Math.floor(width / brickW);
+  let padding = width / cols - brickW;
+  let rows = 12;
+  let offsetX = 0;
+  let offsetY = 100;
 
-let cols = Math.floor(width / brickW);
-let padding = width / cols - brickW;
-let rows = 12;
-let offsetX = 0;
-let offsetY = 100;
-
-for (let j = 0; j < rows; j++) {
-  console.log(padding);
-  for (let i = 0; i < cols; i++) {
-    bricks.push(
-      new Brick(
-        i * brickW + i * padding + padding / 2,
-        j * brickH + j * padding + padding / 2 + offsetY,
-        brickW,
-        brickH,
-        color
-      )
-    );
-  }
-  if ((j + 1) % 3 === 0) {
-    colorIndex++;
-    color = colors[colorIndex];
+  for (let j = 0; j < rows; j++) {
+    //console.log(padding);
+    for (let i = 0; i < cols; i++) {
+      bricks.push(
+        new Brick(
+          i * brickW + i * padding + padding / 2,
+          j * brickH + j * padding + padding / 2 + offsetY,
+          brickW,
+          brickH,
+          color
+        )
+      );
+    }
+    if ((j + 1) % 3 === 0) {
+      colorIndex++;
+      color = colors[colorIndex];
+    }
   }
 }
+
+//let brick=new Brick(10,10,50,10,'rgba(0,0,255,1)');
 
 //x+y*width
 
@@ -139,6 +177,7 @@ for (let j = 0; j < rows; j++) {
 // }
 
 function update() {
+  let time = Date.now();
   ctx.fillStyle = "rgba(0,0,0,100)";
   ctx.fillRect(0, 0, width, height);
   ship.update();
@@ -147,6 +186,12 @@ function update() {
     brick.update();
   });
   ball.update();
+  frame++;
+  if (time - startTime > 1000) {
+    document.querySelector('h1').innerHTML= ((frame / ((time - startTime) / 1000)).toFixed(1));    
+    startTime = time;
+    frame = 0;
+  }
   requestAnimationFrame(update);
 }
 
@@ -155,14 +200,22 @@ function update() {
 // }, 5000);
 
 update();
-console.log(bricks.length);
+//console.log(bricks.length);
 //setTimeout(()=>{bricks.splice(5,1);console.log(bricks.length)},5000)
 document.addEventListener("pointermove", (e) => {
   ship.x = e.clientX - bounds.left - ship.w / 2;
-  ship.y = e.clientY - bounds.top - ship.h / 2;
+  //ship.y = e.clientY - bounds.top - ship.h / 2;
 });
 
-window.addEventListener("resize", (_e) => {
+document.addEventListener("keydown", (e) => {
+  if (e.key === "r") {
+    ball = null;
+    ship = null;
+    init();
+  }
+});
+
+window.addEventListener("resize", () => {
   bounds = canvas.getBoundingClientRect();
 });
 
